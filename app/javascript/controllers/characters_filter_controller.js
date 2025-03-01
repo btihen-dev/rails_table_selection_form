995d9936bec857a1262f8b2f2d3b16297677f8e1
Bill Tihen <btihen@gmail.com>
2025-03-01 22:44:32 +0100

    fix so commits on character entry without <return>

--- app/javascript/controllers/characters_filter_controller.js
+++ app/javascript/controllers/characters_filter_controller.js
@@ -1,12 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["form"];
-
   filter() {
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
-      this.formTarget.requestSubmit();
+      this.element.requestSubmit();
     }, 200);
   }
 }
